35590f81ffda4ab463d14a06f5a0696587fa267b
Rohit Parihar <rohitparih@gmail.com>
2023-03-15 14:51:11 +0530

    GridView_GridTIle

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -35,22 +35,57 @@ class _HomePageState extends State<HomePage> {
     //final dummyList = List.generate(50, (index) => CatalogItems.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Electronic Store"),
+        title: const Text("Electronic Store"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(7.0),
         child: (CatalogItems.items != null && CatalogItems.items.isNotEmpty)
-            ? ListView.builder(
-                //itemCount: CatalogItems.items.length,
+            // ? ListView.builder(
+            //     //itemCount: CatalogItems.items.length,
+            //     itemCount: CatalogItems.items.length,
+            //     itemBuilder: (context, index) {
+            //       return MyWidget(
+            //         // item: CatalogItems.items[index],
+            //         item: CatalogItems.items[index],
+            //       );
+            //     },
+            //   )
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                ),
                 itemCount: CatalogItems.items.length,
                 itemBuilder: (context, index) {
-                  return MyWidget(
-                    // item: CatalogItems.items[index],
-                    item: CatalogItems.items[index],
-                  );
+                  final item = CatalogItems.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(0.0),
+                        child: (GridTile(
+                          header: Container(
+                              decoration:
+                                  const BoxDecoration(color: Colors.deepPurple),
+                              child: Text(
+                                item.name,
+                                style: TextStyle(color: Colors.white),
+                              )),
+                          child: Image.network(item.image),
+                          footer: Container(
+                              decoration:
+                                  const BoxDecoration(color: Colors.black),
+                              child: Text(
+                                textAlign: TextAlign.right,
+                                item.price.toString(),
+                                style: const TextStyle(color: Colors.white),
+                              )),
+                        )),
+                      ));
                 },
               )
-            : Center(
+            : const Center(
                 child: CircularProgressIndicator(),
               ),
       ),
